c7ce030fc94019d52148aead7d004ec29331130f
Adam Florczak <adam.florczak@me.com>
2018-03-18 15:30:36 +0100

    Better fonts

--- rowerotopia-theme/functions.php
+++ rowerotopia-theme/functions.php
@@ -2,4 +2,11 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+  wp_enqueue_style( 'fonts', 'https://fonts.googleapis.com/css?family=Alegreya:400,700|Lato:400,700&amp;subset=latin-ex' );
+}
+
+add_action( 'wp_enqueue_scripts', 'cleanup_parent_styles', 100 );
+function cleanup_parent_styles() {
+  wp_dequeue_style( 'twentyseventeen-fonts' );
+  wp_deregister_style( 'twentyseventeen-fonts' );
 }
