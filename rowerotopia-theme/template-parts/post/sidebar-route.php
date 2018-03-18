947fd3e3eef58e63af2161e09f88e126ed8f2108
Adam Florczak <adam.florczak@me.com>
2018-03-18 14:58:36 +0100

    Wider content area + custom sidebar for route posts

---
+++ rowerotopia-theme/template-parts/post/sidebar-route.php
@@ -0,0 +1,3 @@
+<aside id="secondary" class="widget-area" role="complementary" aria-label="<?php esc_attr_e( 'Blog Sidebar', 'twentyseventeen' ); ?>">
+  <?php get_template_part( 'trip-stats-widget/trip-stats-widget' ); ?>
+</aside>
